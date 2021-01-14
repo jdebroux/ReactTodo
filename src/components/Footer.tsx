c0fe9a894e6496d74961ca9c5b1893d8c416f22c
jdebroux <jtdebroux@gmail.com>
2021-01-13 21:51:17 -0700

    First Commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <h1>This is the footer!</h1>
+        )
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TodoApp from './TodoApp';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+      integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+      crossOrigin="anonymous"
+    />
+    <TodoApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
